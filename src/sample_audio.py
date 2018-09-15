ce27c39ca5f076cfeaaaf5d1945b13270ff65fa3
Elton Lin <eltonlin1998@gmail.com>
2018-09-15 17:10:55 -0400

    Record() returns an array of amplitudes

--- src/sample_audio.py
+++ src/sample_audio.py
@@ -1,3 +1,18 @@
-# input 
+# input: builtin microphone 
 
-# output: np array of amplitude samples 
+def flatten_array(array): 
+	output = [] 
+	for elem in array: 
+		output.append(elem[0])
+	return output
+
+def record(duration=5, fs=44100):
+	import sounddevice as sd
+
+	recording = sd.rec(int(duration * fs), samplerate=fs, channels=1)
+	sd.wait()
+	return flatten_array(recording)
+ 
+if __name__ == "__main__":
+	arr = record()
+	print(arr)
